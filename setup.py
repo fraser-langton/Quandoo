dd102df54006e78f1b6ba779109df3b3974ee972
fraser-langton <fraserbasil@gmail.com>
2019-05-27 23:47:19 +1000

    1.2.1

--- setup.py
+++ setup.py
@@ -1,39 +1,21 @@
-from setuptools import setup, find_packages
-import os
+import setuptools
 
-INSTALL_REQUIRES = ['requests']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-license='MIT'
-if os.path.exists('LICENSE'):
-    license = open('LICENSE').read()
-
-long_description = """
-    Unofficial Quandoo APIs allow Quandoo agents to retrieve and edit their merchant data
-  """
-
-setup(name='Quandoo',
-      version='1.1.0',
-      description='Quandoo API',
-      long_description=long_description,
-      author='Fraser Langton',
-      author_email='fraserbasil@gmail.com',
-      url='https://github.com/fraser-langton/Quandoo',
-      download_url='https://github.com/fraser-langton/Quandoo',
-      license=license,
-      packages=find_packages(),
-      classifiers=[
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        ],
-        install_requires=INSTALL_REQUIRES
-      )
+setuptools.setup(
+    name="Quandoo",
+    version="1.2.1",
+    author="Fraser Langton",
+    author_email="fraserbasil@gmail.com",
+    description="This is a fairly lightwieght SDK for interacting with the Quandoo API, it is a work in progress.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fraser-langton/Quandoo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
